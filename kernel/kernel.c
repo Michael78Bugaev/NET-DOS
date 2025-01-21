164de4c561645aa2fd432ef7d085244ddf8b481e
Michael78Bugaev <mihailbugaev78@gmail.com>
2025-01-21 18:37:29 +0300

    implementing GDT struct

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,8 @@
 #include <80x50vga.h>
+#include <gdt.h>
 
 void kernel_main(void) {
-    vgaprint("NET-DOS VGA 80x25 driver testing...");
+    vgaprint("NET-DOS VGA 80x25 driver testing...\n");
+    gdt_init();
+    vgaprint("GDT initialized");
 }
